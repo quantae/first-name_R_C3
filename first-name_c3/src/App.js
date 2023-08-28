bb8a4bed495ad2448a5f11181c45008e89c11bc9
Gerald O-A, quantae <onegerrald@gmail.com>
2023-08-28 19:17:22 +0000

    finished assignment

---
+++ first-name_c3/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>I am Geral Ofori-Atta and this is my first React application</p>
+    </div>
+  );
+}
+
+export default App;
